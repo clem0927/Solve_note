96ee1bca906d9a155c1c1cf914ee1558441c8b18
clem0927 <minyoung09277@naver.com>
2026-03-13 01:43:15 +0900

    test actions2

--- vite/src/pages/Home.jsx
+++ vite/src/pages/Home.jsx
@@ -279,7 +279,7 @@ const Home = () => {
 
             <aside className="sidebar">
                 <div className="logo-area">
-                    <div className="logo">Solve Notes</div>
+                    <div className="logo">Solve Notess</div>
                     <img className="logo-img" src="images/solvenote.png" />
                 </div>
                 <div
