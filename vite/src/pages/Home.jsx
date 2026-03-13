3565d73d515ee67a0a90be7387b700a1a3aff3eb
clem0927 <minyoung09277@naver.com>
2026-03-13 12:25:54 +0900

    ci cd2

--- vite/src/pages/Home.jsx
+++ vite/src/pages/Home.jsx
@@ -279,7 +279,7 @@ const Home = () => {
 
             <aside className="sidebar">
                 <div className="logo-area">
-                    <div className="logo">Solve NotesSsss</div>
+                    <div className="logo">Solve NotesSssss</div>
                     <img className="logo-img" src="images/solvenote.png" />
                 </div>
                 <div
